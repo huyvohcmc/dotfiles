8fe215787f2a468b723bb097ae07722301f4ad81
Huy Vo <huyvohcmc@gmail.com>
2018-05-21 21:20:49 +0700

    Add gitignore file

--- install.sh
+++ install.sh
@@ -5,12 +5,14 @@ echo 'Starting...'
 # Remove default config files if they're already exist
 [ -f ~/.zshrc ] && rm -rf ~/.zshrc
 [ -f ~/.gitconfig ] && rm -rf ~/.gitconfig
+[ -f ~/.gitignore ] && rm -rf ~/.gitignore
 [ -f ~/.tmux.conf ] && rm -rf ~/.tmux.conf
 [ -f ~/.config/nvim/init.vim ] && rm -rf ~/.config/nvim/init.vim
 
 # Create symbolic links
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/init.vim ~/.config/nvim/init.vim
 
