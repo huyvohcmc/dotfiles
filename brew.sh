63a03e6635f176541da95314b371ca738e61657e
Huy Vo <huyvohcmc@gmail.com>
2018-07-08 23:43:58 +0700

    Add brew script

---
+++ brew.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+# Install all the requirements
+brew install vim --with-override-system-vi
+brew install neovim
+brew install tmux
+brew install tmate
+brew install git
+brew install tig
+brew install zsh
+brew install universal-ctags
+brew install the_silver_searcher
+
+# Remove outdated versions from the cellar
+brew cleanup
