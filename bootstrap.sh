11eca242127bc6c9550d859d5923f47da31ccae5
Huy Vo <huyvohcmc@gmail.com>
2018-07-08 21:16:45 +0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+function install() {
+  ln -sf ~/dotfiles/zshrc ~/.zshrc
+  ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+  ln -sf ~/dotfiles/gitignore ~/.gitignore
+  ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+  ln -sf ~/dotfiles/tigrc ~/.tigrc
+  ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
+  echo "Dotfiles have been installed!"
+}
+
+function update() {
+  cd ~/dotfiles/ && git pull origin master
+  echo "Dotfiles have been updated!"
+}
+
 function backup() {
   mkdir -p ~/dotfiles.backup/
   [ -f ~/.zshrc ] && cp $(realpath ~/.zshrc) ~/dotfiles.backup/
@@ -11,37 +26,23 @@ function backup() {
   echo "Dotfiles have been backed up!"
 }
 
-function install() {
-  ln -sf ~/.dotfiles/zshrc ~/.zshrc
-  ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
-  ln -sf ~/.dotfiles/gitignore ~/.gitignore
-  ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
-  ln -sf ~/.dotfiles/tigrc ~/.tigrc
-  ln -sf ~/.dotfiles/init.vim ~/.config/nvim/init.vim
-  echo "Dotfiles have been installed!"
-}
-
-function update() {
-  cd ~/.dotfiles/ && git pull origin master
-  echo "Dotfiles have been updated!"
-}
-
 case "$1" in
 "")
   read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
   echo "";
   if [[ $REPLY =~ ^[Yy]$ ]]; then
-    install
+    backup;
+    install;
   fi
-  ;;
-"--backup" | "-b")
-  backup
   ;;
 "--install" | "-i")
   install
   ;;
 "--update" | "-u")
   update
+  ;;
+"--backup" | "-b")
+  backup
   ;;
 *)
   echo "No manual entry for $1"
