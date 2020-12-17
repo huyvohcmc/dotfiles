5b3f9a55b351bf28f1796c2e1011459f6cc84a6d
Huy Vo <huyvohcmc@gmail.com>
2020-12-17 17:34:28 +0700

    nvim: update ctags

--- nvim/lua/ctags.lua
+++ nvim/lua/ctags.lua
@@ -1,10 +1,17 @@
 local api = vim.api
 
-api.nvim_exec(([[
-command! Tags !ctags -R -I EXTERN -I INIT --exclude="build*" --exclude=".vim-src/**" --exclude="node_modules/**" --exclude="venv/**"
-   --exclude="**/site-packages/**" --exclude="data/**" --exclude="dist/**" --exclude="notebooks/**" --exclude="notebooks/**"
-   --exclude="*graphhopper_data/*.json" --exclude="*graphhopper/*.json" --exclude="*.json" --exclude="qgis/**" --exclude=.git
-   --exclude=.svn --exclude=.hg --exclude="*.cache.html" --exclude="*.nocache.js" --exclude="*.min.*" --exclude="*.map"
-   --exclude="*.swp" --exclude="*.bak" --exclude="*.pyc" --exclude="*.class" --exclude="*.sln" --exclude="*.master" --exclude="*.csproj"
-   --exclude="*.csproj.user" --exclude="*.cache" --exclude="*.dll" --exclude="*.pdb" --exclude=tags --exclude="cscope.*" --exclude="*.tar.*"
-]]):gsub("\n", ""), '')
+local command = 'command! Tags !ctags -R -I EXTERN -I INIT'
+command = command .. ' ' .. '--exclude=.git --exclude=.svn --exclude=.hg'
+command = command .. ' ' .. '--exclude=.github --exclude=.circleci --exclude="*.swp"'
+command = command .. ' ' .. '--exclude="build/**" --exclude="**/build/**" --exclude="db/**" --exclude="doc/**"'
+command = command .. ' ' .. '--exclude="log/**" --exclude="tmp/**" --exclude="vendor/**"'
+command = command .. ' ' .. '--exclude="*.json" --exclude="*.lock" --exclude="*.md"'
+command = command .. ' ' .. '--exclude="*.yml" --exclude="*.rdb" --exclude="*.log"'
+command = command .. ' ' .. '--exclude="*.dump" --exclude="*.pdb" --exclude="*.dll"'
+command = command .. ' ' .. '--exclude="*.pyc" --exclude="*.class" --exclude="*.o"'
+command = command .. ' ' .. '--exclude=tags --exclude="cscope.*" --exclude="*.cache"'
+command = command .. ' ' .. '--exclude="*.csproj" --exclude="*.csproj.user" --exclude="*.bak"'
+command = command .. ' ' .. '--exclude="assets/**" --exclude="**/assets/**" --exclude="*.min.*" --exclude="*images*"'
+command = command .. ' ' .. '--exclude="dist/**" --exclude="notebooks/**" --exclude="qgis/**"'
+
+api.nvim_exec(command, '')
