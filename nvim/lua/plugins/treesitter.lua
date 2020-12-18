45d65d51790fdf6080112694c6e47dab91bba63f
Huy Vo <huyvohcmc@gmail.com>
2020-12-18 21:12:19 +0700

    treesitter: enable indentation

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,              -- false will disable the whole extension
   },
-  -- indent = {
-  --   enable = true,
-  -- }
+  indent = {
+    enable = true,
+  }
 }
