f3d9812456e5d48f53431180b30edf4fce5e8d2e
Huy Vo <huyvohcmc@gmail.com>
2021-03-11 13:05:43 +0700

    nvim: simplify telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,8 +3,14 @@ local cmd = vim.cmd
 local api = vim.api
 
 local telescope = require('telescope')
+local actions = require('telescope.actions')
 telescope.setup{
     defaults = {
+        mapping = {
+          i = {
+            ["<esc>"] = actions.close,
+          }
+        },
         vimgrep_arguments = {
             'rg',
             '--color=never',
@@ -14,31 +20,9 @@ telescope.setup{
             '--column',
             '--smart-case'
         },
-        prompt_position = "top",
-        prompt_prefix = ">",
-        selection_strategy = "reset",
         sorting_strategy = "ascending",
-        layout_strategy = "horizontal",
-        layout_defaults = {
-            -- TODO add builtin options.
-        },
-        file_sorter =  require'telescope.sorters'.get_fuzzy_file,
+        prompt_position = "top",
         file_ignore_patterns = {'node_modules', 'flow%-typed'},
-        generic_sorter =    require'telescope.sorters'.get_generic_fuzzy_sorter,
-        shorten_path = true,
-        winblend = 0,
-        width = 0.75,
-        preview_cutoff = 120,
-        results_height = 1,
-        results_width = 0.8,
-        border = {},
-        borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰'},
-        color_devicons = true,
-        use_less = true,
-        set_env = { ['COLORTERM'] = 'truecolor' }, -- default { }, currently unsupported for shells like cmd.exe / powershell.exe
-        file_previewer = require'telescope.previewers'.vim_buffer_cat.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_cat.new`
-        grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_vimgrep.new`
-        qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_qflist.new`
     }
 }
 
