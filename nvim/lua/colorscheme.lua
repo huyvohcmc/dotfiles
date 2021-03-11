5d1f04d0c96d86e457b633e197309237b5a875a6
Huy Vo <huyvohcmc@gmail.com>
2021-03-11 13:09:41 +0700

    nvim: remove comments

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -4,12 +4,6 @@ local cmd = vim.cmd
 local setup = function()
   g.equinusocio_material_style = 'pure'
   cmd('colorscheme equinusocio_material')
-  -- vim.cmd('hi! Normal guibg=NONE')
-  -- vim.cmd('hi! Visual guibg=#9EC49F guifg=#050505')
-  -- vim.cmd('hi! SignColumn guibg=NONE')
-  -- vim.cmd('hi! StatusLine guifg=white')
-  -- vim.cmd('hi! Comment guifg=#545454')
-  -- vim.cmd('hi! VertSplit guibg=NONE')
 end
 
 setup()
