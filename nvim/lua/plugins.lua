8f1daea0e251baf278706608be35aa8356000634
Huy Vo <huyvohcmc@gmail.com>
2021-03-12 10:39:17 +0700

    nvim: use fzy native sorter

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -37,7 +37,11 @@ return require('packer').startup(function()
   use 'justinmk/vim-dirvish'
   use {
     'nvim-telescope/telescope.nvim',
-    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+    requires = {
+      {'nvim-lua/popup.nvim'},
+      {'nvim-lua/plenary.nvim'},
+      {'nvim-telescope/telescope-fzy-native.nvim'}
+    }
   }
 
   use {
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -23,8 +23,15 @@ telescope.setup{
         sorting_strategy = "ascending",
         prompt_position = "top",
         file_ignore_patterns = {'node_modules', 'flow%-typed'},
+    },
+    extensions = {
+        fzy_native = {
+            override_generic_sorter = true,
+            override_file_sorter = true,
+        }
     }
 }
+require('telescope').load_extension('fzy_native')
 
 -- disable deoplete
 cmd"autocmd FileType TelescopePrompt call deoplete#custom#buffer_option('auto_complete', v:false)"
