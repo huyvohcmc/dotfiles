78a13a2c53dafead8a18d6a1cf2fb13a62fc093f
Huy Vo <huyvohcmc@gmail.com>
2021-03-12 19:52:31 +0700

    nvim: remove unused code

--- nvim/lua/plugins/signify.lua
+++ nvim/lua/plugins/signify.lua
@@ -5,6 +5,3 @@ g.signify_vcs_list = { 'git' }
 g.signify_realtime = 1
 g.signify_sign_show_count = 0
 g.signify_sign_change = '~'
-
-cmd('hi! SignifySignChange guifg=#F7F6AF')
-cmd('hi! SignifySignDelete guifg=#F7AFC0')
