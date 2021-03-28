c89de1919ebf05b54019a0119d3098e50b6f0497
Huy Vo <huyvohcmc@gmail.com>
2021-03-28 19:23:07 +0700

    nvim: change git blame message template

--- nvim/lua/plugins/git-blame.lua
+++ nvim/lua/plugins/git-blame.lua
@@ -1,3 +1,8 @@
-vim.g.gitblame_enabled = 0
+local g = vim.g
+local api = vim.api
 
-vim.api.nvim_set_keymap('n', 'gb', ':GitBlameToggle<CR>', { noremap = true, silent = false })
+g.gitblame_enabled = 0
+g.gitblame_date_format = '%d/%m/%Y'
+g.gitblame_message_template = '<sha> • <summary> • <author> • <date>'
+
+api.nvim_set_keymap('n', 'gb', ':GitBlameToggle<CR>', { noremap = true, silent = false })
