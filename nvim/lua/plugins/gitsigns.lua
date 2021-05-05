4b204f08fccff69c6486b9ba5871eb556635d528
Huy Vo <huyvohcmc@gmail.com>
2021-05-05 17:14:24 +0700

    Add gitsigns setup

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,1 +1,9 @@
-  require'gitsigns'.setup()
+  require'gitsigns'.setup{
+    signs = {
+      add          = { text= '┃' },
+      change       = { text= '┃' },
+      delete       = { text= '_' },
+      topdelete    = { text= '‾' },
+      changedelete = { text= '≃' },
+    }
+  }
