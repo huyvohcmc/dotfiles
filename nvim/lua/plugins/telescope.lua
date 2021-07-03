909213628f14988b0e55283a85e59c44da43d39c
Huy Vo <huyvohcmc@gmail.com>
2021-07-03 21:01:56 +0700

    nvim: update telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,44 +5,61 @@ require("telescope").setup {
   defaults = {
     prompt_prefix = "❯ ",
     selection_caret = "❯ ",
+
     winblend = 0,
+
     layout_strategy = "horizontal",
     layout_config = {
-      width = 0.8,
+      width = 0.95,
       height = 0.85,
-      preview_cutoff = 120,
+      -- preview_cutoff = 120,
+      prompt_position = "top",
+
       horizontal = {
-        preview_width = 0.6,
+        preview_width = function(_, cols, _)
+          if cols > 200 then
+            return math.floor(cols * 0.4)
+          else
+            return math.floor(cols * 0.6)
+          end
+        end,
       },
+
       vertical = {
         width = 0.9,
         height = 0.95,
         preview_height = 0.5,
       },
+
       flex = {
         horizontal = {
           preview_width = 0.9,
         },
       },
     },
+
     selection_strategy = "reset",
-    prompt_position = "top",
     sorting_strategy = "ascending",
     scroll_strategy = "cycle",
-    color_devicons = true,
+    color_devicons = false,
+
     mappings = {
       i = {
         ["<C-x>"] = false,
         ["<C-s>"] = actions.select_horizontal,
       },
     },
+
     borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+
     file_sorter = sorters.get_fzy_sorter,
     file_ignore_patterns = {'.git', 'node_modules', 'flow%-typed'},
+
     file_previewer = require("telescope.previewers").vim_buffer_cat.new,
     grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
     qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
   },
+
   extensions = {
     fzy_native = {
       override_generic_sorter = true,
