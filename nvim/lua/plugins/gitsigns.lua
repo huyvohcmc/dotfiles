394b74aca1ff6549e1cbe7eca1fee3d55baeb208
Huy Vo <huyvohcmc@gmail.com>
2021-07-24 18:38:35 +0700

    nvim: update gitsigns config

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,9 +1,34 @@
-  require'gitsigns'.setup{
-    signs = {
-      add          = { hl = 'GitGutterAdd', text = '▋' },
-      change       = { hl = 'GitGutterChange', text = '▋' },
-      delete       = { hl = 'GitGutterDelete', text = '▋' },
-      topdelete    = { hl = 'GitGutterDeleteChange', text = '▔' },
-      changedelete = { hl = 'GitGutterChange', text = '▎' },
-    }
-  }
+require("gitsigns").setup({
+  signs = {
+    add = {
+      hl = "GitSignsAdd",
+      text = "▋",
+      numhl = "GitSignsAddNr",
+      linehl = "GitSignsAddLn"
+    },
+    change = {
+      hl = "GitSignsChange",
+      text = "▋",
+      numhl = "GitSignsChangeNr",
+      linehl = "GitSignsChangeLn",
+    },
+    delete = {
+      hl = "GitSignsDelete",
+      text = "▸",
+      numhl = "GitSignsDeleteNr",
+      linehl = "GitSignsDeleteLn",
+    },
+    topdelete = {
+      hl = "GitSignsDelete",
+      text = "▾",
+      numhl = "GitSignsDeleteNr",
+      linehl = "GitSignsDeleteLn",
+    },
+    changedelete = {
+      hl = "GitSignsChange",
+      text = "▋",
+      numhl = "GitSignsChangeNr",
+      linehl = "GitSignsChangeLn",
+    },
+  },
+})
