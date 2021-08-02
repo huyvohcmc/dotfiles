f5c0cad86803af9965b91c19cbad7cf3019147b5
Huy Vo <huyvohcmc@gmail.com>
2021-08-02 12:50:27 +0700

    Replace double quotes with single quotes

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,34 +1,34 @@
-require("gitsigns").setup({
+require('gitsigns').setup({
   signs = {
     add = {
-      hl = "GitSignsAdd",
-      text = "▋",
-      numhl = "GitSignsAddNr",
-      linehl = "GitSignsAddLn"
+      hl = 'GitSignsAdd',
+      text = '▋',
+      numhl = 'GitSignsAddNr',
+      linehl = 'GitSignsAddLn'
     },
     change = {
-      hl = "GitSignsChange",
-      text = "▋",
-      numhl = "GitSignsChangeNr",
-      linehl = "GitSignsChangeLn",
+      hl = 'GitSignsChange',
+      text = '▋',
+      numhl = 'GitSignsChangeNr',
+      linehl = 'GitSignsChangeLn',
     },
     delete = {
-      hl = "GitSignsDelete",
-      text = "▸",
-      numhl = "GitSignsDeleteNr",
-      linehl = "GitSignsDeleteLn",
+      hl = 'GitSignsDelete',
+      text = '▸',
+      numhl = 'GitSignsDeleteNr',
+      linehl = 'GitSignsDeleteLn',
     },
     topdelete = {
-      hl = "GitSignsDelete",
-      text = "▾",
-      numhl = "GitSignsDeleteNr",
-      linehl = "GitSignsDeleteLn",
+      hl = 'GitSignsDelete',
+      text = '▾',
+      numhl = 'GitSignsDeleteNr',
+      linehl = 'GitSignsDeleteLn',
     },
     changedelete = {
-      hl = "GitSignsChange",
-      text = "▋",
-      numhl = "GitSignsChangeNr",
-      linehl = "GitSignsChangeLn",
+      hl = 'GitSignsChange',
+      text = '▋',
+      numhl = 'GitSignsChangeNr',
+      linehl = 'GitSignsChangeLn',
     },
   },
 })
