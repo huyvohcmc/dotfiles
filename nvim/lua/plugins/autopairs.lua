d0bc943c371b415361642aaacddf6b4ee9aa681d
Huy Vo <huyvohcmc@gmail.com>
2021-08-09 12:32:40 +0700

    Enable end-wise behavior

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,3 +1,10 @@
 local npairs = require('nvim-autopairs')
 
 npairs.setup()
+
+npairs.add_rules(require('nvim-autopairs.rules.endwise-ruby'))
+
+require('nvim-autopairs.completion.compe').setup({
+  map_cr = true,
+  map_complete = true,
+})
