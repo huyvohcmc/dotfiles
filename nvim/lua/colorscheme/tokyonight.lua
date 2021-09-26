164a2b3387d193de028424415fffb5f83163950c
Huy Vo <huyvohcmc@gmail.com>
2021-09-26 19:25:26 +0700

    Update tokyonight

--- nvim/lua/colorscheme/tokyonight.lua
+++ nvim/lua/colorscheme/tokyonight.lua
@@ -1,4 +1,9 @@
 vim.g.tokyonight_style = 'night'
+vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 vim.g.tokyonight_dark_sidebar = true
 vim.g.tokyonight_dark_float = true
+vim.g.tokyonight_colors = {
+  bg_sidebar = '#1a1b26',
+}
+
 vim.cmd[[colorscheme tokyonight]]
