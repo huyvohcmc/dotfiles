57a354fa740754163fe7596dee86a3d6cf19db82
Huy Vo <huyvohcmc@gmail.com>
2021-09-26 19:25:14 +0700

    Update nvim tree config

--- nvim/lua/plugins/nvim_tree.lua
+++ nvim/lua/plugins/nvim_tree.lua
@@ -1,17 +1,8 @@
 local g = vim.g
 local api = vim.api
 
-g.nvim_tree_ignore = { '.git', 'node_modules', 'dist' }
-g.nvim_tree_auto_open = 1
-g.nvim_tree_auto_close = 0
-g.nvim_tree_auto_ignore_ft = { 'dashboard' }
-g.nvim_tree_tab_open = 1
 g.nvim_tree_quit_on_open = 1
-g.nvim_tree_disable_netrw = 0
-g.nvim_tree_hijack_netrw = 1
-g.nvim_tree_follow = 1
 g.nvim_tree_gitignore = 1
-g.nvim_tree_update_cwd = 1
 g.nvim_tree_show_icons = {
   git = 0,
   folders = 0,
@@ -19,8 +10,6 @@ g.nvim_tree_show_icons = {
   folder_arrows = 1
 }
 g.nvim_tree_group_empty = 1
-g.nvim_tree_lsp_diagnostics = 1
-g.nvim_tree_hijack_cursor = 1
 g.nvim_tree_window_picker_exclude = {
   filetype = {
     "packer",
@@ -28,7 +17,26 @@ g.nvim_tree_window_picker_exclude = {
     "Trouble"
   }
 }
-
 api.nvim_set_keymap('n', '-', ':NvimTreeFindFile<CR>', {noremap = true, silent = true})
 api.nvim_set_keymap('n', '<leader>n', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
 api.nvim_set_keymap('n', '<leader>r', ':NvimTreeRefresh<CR>', {noremap = true, silent = true})
+
+require'nvim-tree'.setup {
+  disable_netrw       = true,
+  hijack_netrw        = true,
+  open_on_setup       = false,
+  lsp_diagnostics     = false,
+  auto_close          = false,
+  hijack_cursor       = true,
+  update_cwd          = true,
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+    ignore_list = { 'fzf' }
+  },
+  ignore_ft_on_setup = {'.git', 'node_modules', 'dist'},
+  system_open = {
+    cmd  = nil,
+    args = {}
+  },
+}
