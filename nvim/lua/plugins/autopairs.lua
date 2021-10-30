eb6f1b88120699592c2f4733c2393d3331ac8d9d
Huy Vo <huyvohcmc@gmail.com>
2021-10-30 18:20:03 +0700

    Update config

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,16 +1,9 @@
 local npairs = require('nvim-autopairs')
 npairs.setup {}
 
-require("nvim-autopairs.completion.cmp").setup({
-  map_cr = true, --  map <CR> on insert mode
-  map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
-  auto_select = true, -- automatically select the first item
-  insert = false, -- use insert confirm behavior instead of replace
-  map_char = { -- modifies the function or method delimiter by filetypes
-    all = '(',
-    tex = '{'
-  },
-})
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
 
 npairs.add_rules(require('nvim-autopairs.rules.endwise-ruby'))
 npairs.add_rules(require('nvim-autopairs.rules.endwise-lua'))
