c6e3d32e0a81433d1f5d69b93c730e21c0928479
Huy Vo <huyvohcmc@gmail.com>
2021-12-23 21:54:15 +0700

    Update nvim-tree config

--- nvim/lua/plugins/nvim_tree.lua
+++ nvim/lua/plugins/nvim_tree.lua
@@ -1,7 +1,6 @@
 local g = vim.g
 local api = vim.api
 
-g.nvim_tree_quit_on_open = 1
 g.nvim_tree_show_icons = {
   git = 0,
   folders = 0,
@@ -30,14 +29,23 @@ require'nvim-tree'.setup {
   update_focused_file = {
     enable = true,
     update_cwd = true,
-    ignore_list = { 'fzf' }
+    ignore_list = { 'fzf', 'help', 'git' }
   },
-  ignore_ft_on_setup = {'.git', 'node_modules', 'dist'},
+  ignore_ft_on_setup = { 'git', 'man', 'help' },
   system_open = {
     cmd  = nil,
     args = {}
   },
+  filters = {
+    dotfiles = false,
+    custom = { '.git', 'node_modules', 'dist', '.DS_Store' }
+  },
+  view = {
+    auto_resize = true,
+  },
   git = {
+    enable = true,
     ignore = true,
+    timeout = 400,
   },
 }
