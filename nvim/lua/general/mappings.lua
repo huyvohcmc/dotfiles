da1e39701d188b9f551f1e392edb7efb123f30a3
Huy Vo <huyvohcmc@gmail.com>
2022-04-18 23:12:09 +0700

    Remap for dealing with word wrap

--- nvim/lua/general/mappings.lua
+++ nvim/lua/general/mappings.lua
@@ -3,6 +3,7 @@ local set = vim.keymap.set
 set('n', '<leader>w', ':w<cr>', { silent = false })
 set('n', '<leader>q', ':q<cr>', { silent = false })
 set('n', '<leader>s', '<c-w>w', { silent = false })
-set('n', 'j', 'gj')
-set('n', 'k', 'gk')
 set({'n', 'v'}, '<Tab>', '%')
+
+set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
