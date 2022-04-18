3082d3eb6cc3588c6c281143df405921f4d80317
Huy Vo <huyvohcmc@gmail.com>
2022-04-18 23:08:11 +0700

    Highlight on yank

--- nvim/lua/general/commands.lua
+++ nvim/lua/general/commands.lua
@@ -2,3 +2,12 @@ local api = vim.api
 
 local window_resize_group = api.nvim_create_augroup('WindowResize', { clear = true })
 api.nvim_create_autocmd('VimResized', { command = 'wincmd =', group = window_resize_group })
+
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
