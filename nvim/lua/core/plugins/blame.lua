cf2bbc07261d585149b784b5ae2f6b6d6f890779
Huy Vo <huyvohcmc@gmail.com>
2022-07-27 11:46:46 +0700

    Change git blame toggle mapping

--- nvim/lua/core/plugins/blame.lua
+++ nvim/lua/core/plugins/blame.lua
@@ -4,4 +4,4 @@ g.gitblame_enabled = 0
 g.gitblame_date_format = '%d/%m/%Y'
 g.gitblame_message_template = ' <author>, <date> - <summary>'
 
-vim.keymap.set('n', '<leader>gb', ':GitBlameToggle<cr>', { silent = false })
+vim.keymap.set('n', 'gb', ':GitBlameToggle<cr>', { silent = false })
