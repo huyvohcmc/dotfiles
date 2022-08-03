8bd1afa4fd634db015f92092c29798604f9e0be4
Huy Vo <huyvohcmc@gmail.com>
2022-08-03 16:53:49 +0700

    Remove window resize autocmd

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -1,8 +1,5 @@
 local api = vim.api
 
-local window_resize_group = api.nvim_create_augroup('WindowResize', { clear = true })
-api.nvim_create_autocmd('VimResized', { command = 'wincmd =', group = window_resize_group })
-
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
