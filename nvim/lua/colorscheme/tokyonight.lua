91dc8768338385529e110337ce308580a55f31c6
Huy Vo <huyvohcmc@gmail.com>
2022-08-12 10:52:57 +0700

    Tweak theme

--- nvim/lua/colorscheme/tokyonight.lua
+++ nvim/lua/colorscheme/tokyonight.lua
@@ -6,7 +6,7 @@ g.tokyonight_sidebars = { 'qf', 'terminal', 'packer' }
 g.tokyonight_dark_sidebar = true
 g.tokyonight_dark_float = true
 
-api.nvim_set_hl(0, 'LspDiagnosticsLineNrError', { fg = '#db4b4b', bg = '#3b4261' })
-api.nvim_set_hl(0, 'LspDiagnosticsLineNrWarning', { fg = '#ff9e64', bg = '#3b4261' })
+api.nvim_set_hl(0, 'LspDiagnosticsLineNrError', { fg = '#db4b4b', bg = NONE })
+api.nvim_set_hl(0, 'LspDiagnosticsLineNrWarning', { fg = '#ff9e64', bg = NONE })
 
 vim.cmd.colorscheme 'tokyonight'
