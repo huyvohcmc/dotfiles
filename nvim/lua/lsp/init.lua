b1e01245607e32c9546e927ac77ba2fd64b32ffd
Huy Vo <huyvohcmc@gmail.com>
2022-10-15 17:19:40 +0700

    Update config

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -44,7 +44,7 @@ local on_attach = function(client, bufnr)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 for _, server in ipairs {
   'null-ls',
