42bb14e3be1bb1e379e2965c8ed7316ebcc625f7
Huy Vo <huyvohcmc@gmail.com>
2022-11-05 12:37:53 +0700

    Update config

--- nvim/lua/colorschemes/tokyonight.lua
+++ nvim/lua/colorschemes/tokyonight.lua
@@ -1,10 +1,19 @@
-local g = vim.g
 local api = vim.api
 
-g.tokyonight_style = 'night'
-g.tokyonight_sidebars = { 'qf', 'terminal', 'packer' }
-g.tokyonight_dark_sidebar = true
-g.tokyonight_dark_float = true
+require('tokyonight').setup {
+  style = 'night',
+  transparent = false,
+  terminal_colors = true,
+  styles = {
+    comments = { italic = true },
+    keywords = {},
+    functions = {},
+    variables = {},
+    sidebars = 'dark',
+    floats = 'dark',
+  },
+  sidebars = { 'qf', 'help', 'terminal', 'packer' }
+}
 
 api.nvim_set_hl(0, 'LspDiagnosticsLineNrError', { fg = '#db4b4b', bg = NONE })
 api.nvim_set_hl(0, 'LspDiagnosticsLineNrWarning', { fg = '#ff9e64', bg = NONE })
