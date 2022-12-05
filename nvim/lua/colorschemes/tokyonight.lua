a8ccf13944c42e5b3c0c368b845118e94883a955
Huy Vo <huyvohcmc@gmail.com>
2022-12-06 01:11:26 +0700

    Borderless

--- nvim/lua/colorschemes/tokyonight.lua
+++ nvim/lua/colorschemes/tokyonight.lua
@@ -13,6 +13,36 @@ require('tokyonight').setup {
     floats = 'dark',
   },
   sidebars = { 'qf', 'help', 'terminal', 'packer' },
+  on_highlights = function(hl, c)
+    local prompt = '#2d3149'
+    hl.TelescopeNormal = {
+      bg = c.bg_dark,
+      fg = c.fg_dark,
+    }
+    hl.TelescopeBorder = {
+      bg = c.bg_dark,
+      fg = c.bg_dark,
+    }
+    hl.TelescopePromptNormal = {
+      bg = prompt,
+    }
+    hl.TelescopePromptBorder = {
+      bg = prompt,
+      fg = prompt,
+    }
+    hl.TelescopePromptTitle = {
+      bg = prompt,
+      fg = prompt,
+    }
+    hl.TelescopePreviewTitle = {
+      bg = c.bg_dark,
+      fg = c.bg_dark,
+    }
+    hl.TelescopeResultsTitle = {
+      bg = c.bg_dark,
+      fg = c.bg_dark,
+    }
+  end,
 }
 
 api.nvim_set_hl(0, 'LspDiagnosticsLineNrError', { fg = '#db4b4b', bg = NONE })
