befba897fc0b2298d8d71b885221bbdce474ed64
Huy Vo <huyvohcmc@gmail.com>
2022-12-06 20:43:51 +0700

    Manage hlsearch

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -4,6 +4,16 @@ local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = t
 api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   callback = function()
-    vim.highlight.on_yank { higroup = 'Substitute', timeout = 200 }
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 100 }
   end,
 })
+
+local hl_ns = api.nvim_create_namespace('hl_search')
+local manage_hlsearch = function(char)
+  if vim.fn.mode() == "n" then
+    local new_hlsearch = vim.tbl_contains({ "<CR>", "n", "N", "*", "#", "?", "/" }, vim.fn.keytrans(char))
+    if vim.opt.hlsearch:get() ~= new_hlsearch then vim.opt.hlsearch = new_hlsearch end
+  end
+end
+
+vim.on_key(manage_hlsearch, hl_ns)
