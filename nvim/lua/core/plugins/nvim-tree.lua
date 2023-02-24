f7d8615371b758cb9ea848450fc1f09c4c5859c1
Huy Vo <huyvohcmc@gmail.com>
2023-02-24 22:32:15 +0700

    Open for directories

--- nvim/lua/core/plugins/nvim-tree.lua
+++ nvim/lua/core/plugins/nvim-tree.lua
@@ -10,6 +10,23 @@ function M.config()
   set('n', '<leader>n', ':NvimTreeToggle<CR>')
   set('n', '<leader>r', ':NvimTreeRefresh<CR>')
 
+  local function open_nvim_tree(data)
+    -- buffer is a directory
+    local directory = vim.fn.isdirectory(data.file) == 1
+
+    if not directory then
+      return
+    end
+
+    -- change to the directory
+    vim.cmd.cd(data.file)
+
+    -- open the tree
+    require('nvim-tree.api').tree.open()
+  end
+
+  vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
+
   require('nvim-tree').setup {
     disable_netrw = true,
     hijack_netrw = true,
