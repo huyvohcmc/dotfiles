a07095e54f9a0850bdaf187332a6b17eb8eb5103
Huy Vo <huyvohcmc@gmail.com>
2023-06-03 14:06:45 +0700

    Update config

--- nvim/lua/core/plugins/colorizer.lua
+++ nvim/lua/core/plugins/colorizer.lua
@@ -1,11 +1,4 @@
 return {
-  'NvChad/nvim-colorizer.lua',
+  'norcalli/nvim-colorizer.lua',
   event = 'BufReadPre',
-  config = function()
-    require('colorizer').setup {
-      user_default_options = {
-        names = false,
-      },
-    }
-  end,
 }
